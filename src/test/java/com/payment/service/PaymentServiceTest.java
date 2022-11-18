158e9424960b2d65756de5cc7e8ebabc11859161
Gabriela Linck <gabriela.jesus@dafiti.com.br>
2022-11-18 11:54:13 -0300

    add tests to PaymentService

--- src/test/java/com/payment/service/PaymentServiceTest.java
+++ src/test/java/com/payment/service/PaymentServiceTest.java
@@ -15,18 +15,22 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 @SpringBootTest
+@Transactional
 class PaymentServiceTest {
 
     public static final int USER_ID = 1;
     public static final int ACCOUNT_ID = 1;
     public static final int PRODUCT_ID_1 = 1;
+    public static final int PRODUCT_ID_2 = 2;
     @Resource
     private PaymentRepository paymentRepository;
 
@@ -50,24 +54,63 @@ class PaymentServiceTest {
         BigDecimal totalPrice = product1.getPrice();
         PaymentRequest paymentRequest = new PaymentRequest();
         paymentRequest.setProducts(products);
-        when(paymentService.createPayment(USER_ID, ACCOUNT_ID, paymentRequest)).thenReturn(buildPaymenyt(products, totalPrice));
+        when(paymentService.createPayment(USER_ID, ACCOUNT_ID, paymentRequest))
+                .thenReturn(buildPaymenyt(products, totalPrice).getId());
 
         Integer actual = paymentService.createPayment(USER_ID, ACCOUNT_ID, paymentRequest);
-        Integer expected = buildPaymenyt(products, totalPrice);
+        Integer expected = buildPaymenyt(products, totalPrice).getId();
 
         assertEquals(expected, actual);
     }
 
-    private Integer buildPaymenyt(List<Product> products, BigDecimal totalPrice) {
+    @Test
+    void shouldBuildPaymentOf2Products() {
+
+        Product product1 = productRepository.findById(PRODUCT_ID_1).orElseThrow(()-> new RuntimeException());
+        Product product2 = productRepository.findById(PRODUCT_ID_2).orElseThrow(()-> new RuntimeException());
+        List<Product> products = new ArrayList<Product>(){};
+        products.add(product1);
+        products.add(product2);
+        BigDecimal totalPrice = product1.getPrice().add(product2.getPrice());
+        PaymentRequest paymentRequest = new PaymentRequest();
+        paymentRequest.setProducts(products);
+        when(paymentService.createPayment(USER_ID, ACCOUNT_ID, paymentRequest))
+                .thenReturn(buildPaymenyt(products, totalPrice).getId());
+
+        Integer actual = paymentService.createPayment(USER_ID, ACCOUNT_ID, paymentRequest);
+        Integer expected = buildPaymenyt(products, totalPrice).getId();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldConfirmPayment() {
+        Product product1 = productRepository.findById(PRODUCT_ID_1).orElseThrow(()-> new RuntimeException());
+        List<Product> products = new ArrayList<Product>(){};
+        products.add(product1);
+        BigDecimal totalPrice = product1.getPrice();
+        PaymentRequest paymentRequest = new PaymentRequest();
+        paymentRequest.setProducts(products);
+
+        byte[] paymentInBytes = String.valueOf(1).getBytes();
+        String paymentHash = new String(Base64.getEncoder().encodeToString(paymentInBytes));
+        when(paymentService.confirmPayment(paymentHash)).thenReturn(buildPaymenyt(products, totalPrice));
+        Payment payment = paymentService.confirmPayment(paymentHash);
+        assertTrue(payment.isConfirmed());
+    }
+
+    private Payment buildPaymenyt(List<Product> products, BigDecimal totalPrice) {
         User user = userRepository.findById(USER_ID).orElseThrow(()-> new RuntimeException());
         Account account = accountRepository.findById(ACCOUNT_ID).orElseThrow(()-> new RuntimeException());
         Payment payment = new Payment();
+        payment.setId(1);
+        payment.setConfirmed(true);
         payment.setProducts(products);
         payment.setAccount(account);
         payment.setTotalPrice(totalPrice);
         payment.setUser(user);
 
-        return payment.getId();
+        return payment;
     }
 
 }
